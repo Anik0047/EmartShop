67a940667f0368a6dd811be39a064ef239612f62
Anik0047 <clananik@gmail.com>
2024-06-02 22:58:15 +0600

    categories and brand form done

---
+++ admin_area/admin_page/insert_categories.php
@@ -0,0 +1,10 @@
+<h1 class="text-4xl text-center font-bold">Insert Categories</h1>
+<form action="" method="post" class="mt-10 flex flex-col justify-center items-center">
+    <input type="text" name="categories_title" placeholder="Enter Categories"
+        class="input input-bordered border border-black w-2/4 mx-96" />
+    <!-- <input
+        class="text-lg font-bold rounded-lg  py-2 px-10 w-52 mt-5 bg-white hover:bg-orange-500 transition ease-in delay-150 hover:-translate-y-1 hover:ease-in-out hover:scale-110 duration-300"
+        type="submit"> -->
+    <button
+        class="text-lg font-bold rounded-lg  py-2 px-10 w-52 mt-5 bg-white hover:bg-orange-500 transition ease-in delay-150 hover:-translate-y-1 hover:ease-in-out hover:scale-110 duration-300">Submit</button>
+</form>
--- admin_area/index.php
+++ admin_area/index.php
@@ -11,9 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -23,7 +21,7 @@
             <!-- Page content here -->
             <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
             <div class="flex justify-between  items-center bg-gray-300 md:p-8">
-                <h1 class="text-3xl">Admin Dashboard</h1>
+                <h1 class="text-3xl"><a href="index.php">Admin Dashboard</a></h1>
                 <div class="flex items-center md:pe-10">
                     <img class="w-16" src="../images/logo.png" alt="">
                     <p class="text-lg md:ps-5">Anik Barua</p>
@@ -32,7 +30,10 @@
             <div class="md:mt-10">
                 <?php
                 if (isset($_GET['insert_categories'])) {
-                    include('insert_categories.php');
+                    include('./admin_page/insert_categories.php');
+                }
+                if (isset($_GET['insert_brands'])) {
+                    include('./admin_page/insert_brands.php');
                 }
                 ?>
             </div>
@@ -64,10 +65,8 @@
                     </div>
                     <div class="collapse-content text-lg">
                         <a class="block py-5" href=""><i class="fa-solid fa-bolt pe-5"></i>Products</a>
-                        <a class="block py-5" href="index.php?insert_categories"><i
-                                class="fa-solid fa-bolt pe-5"></i>Categories</a>
-                        <a class="block py-5" href="admin_page/insert_categories.php"><i
-                                class="fa-solid fa-bolt pe-5"></i>Brands</a>
+                        <a class="block py-5" href="index.php?insert_categories"><i class="fa-solid fa-bolt pe-5"></i>Categories</a>
+                        <a class="block py-5" href="index.php?insert_brands"><i class="fa-solid fa-bolt pe-5"></i>Brands</a>
                     </div>
                 </div>
                 <div class="collapse collapse-plus">
