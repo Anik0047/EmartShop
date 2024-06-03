90b9615afbfc4e094759b3c21fe6de16d7b7f6ba
Anik0047 <clananik@gmail.com>
2024-06-03 21:14:41 +0600

    select option category and brand updated

--- admin_area/admin_page/insert_product.php
+++ admin_area/admin_page/insert_product.php
@@ -1,3 +1,6 @@
+<?php
+include('.././database/connect.php');
+?>
 <div>
     <h1 class="text-center text-3xl">Insert Products</h1>
     <div class="mt-10">
@@ -7,7 +10,7 @@
                 <div class="mb-5">
                     <label class="form-control w-full max-w-xs">
                         <div class="label">
-                            <span class="label-text">Product Name</span>
+                            <span class="label-text font-bold font-bold">Product Name</span>
                         </div>
                         <input type="text" name="product_name" placeholder="Enter Product Name" class="input input-bordered w-full max-w-xs" autocomplete="off" require />
                     </label>
@@ -15,7 +18,7 @@
                 <div class="mb-5">
                     <label class="form-control w-full max-w-xs">
                         <div class="label">
-                            <span class="label-text">Product Description</span>
+                            <span class="label-text font-bold">Product Description</span>
                         </div>
                         <input type="text" name="product_descriptipm" placeholder="Enter Description" class="input input-bordered w-full max-w-xs" autocomplete="off" require />
                     </label>
@@ -23,7 +26,7 @@
                 <div class="mb-5">
                     <label class="form-control w-full max-w-xs">
                         <div class="label">
-                            <span class="label-text">Product Keywords</span>
+                            <span class="label-text font-bold">Product Keywords</span>
                         </div>
                         <input type="text" name="product_keywords" placeholder="Enter Keywords" class="input input-bordered w-full max-w-xs" autocomplete="off" required />
                     </label>
@@ -31,19 +34,35 @@
                 <div class="mb-5">
                     <select name="product_categories" class="select select-bordered w-full max-w-xs">
                         <option disabled selected>Select a category</option>
-                        <option>category 1</option>
-                        <option>category 2</option>
-                        <option>category 3</option>
-                        <option>category 4</option>
+                        <?php
+                        // Query to select all categories from the database
+                        $select_categories = "SELECT * from `categories`";
+                        // Execute the query
+                        $categories_result = mysqli_query($conn, $select_categories);
+                        // Loop through the results and display each category
+                        while ($row_data = mysqli_fetch_assoc($categories_result)) {
+                            $category_id = $row_data['category_id'];
+                            $category_title = $row_data['category_title'];
+                            echo "<option value='$category_id'>$category_title</option>";
+                        }
+                        ?>
                     </select>
                 </div>
                 <div class="mb-5">
                     <select name="product_categories" class="select select-bordered w-full max-w-xs">
                         <option disabled selected>Select a brand</option>
-                        <option>brand 1</option>
-                        <option>brand 2</option>
-                        <option>brand 3</option>
-                        <option>brand 4</option>
+                        <?php
+                        // Query to select all brands from the database
+                        $select_brands = "SELECT * from `brands`";
+                        // Execute the query
+                        $brands_result = mysqli_query($conn, $select_brands);
+                        // Loop through the results and display each brand
+                        while ($row_data = mysqli_fetch_assoc($brands_result)) {
+                            $brand_id = $row_data['brands_id'];
+                            $brand_title = $row_data['brands_title'];
+                            echo "<option value='$brand_id'>$brand_title</option>";
+                        }
+                        ?>
                     </select>
                 </div>
             </div>
@@ -51,7 +70,7 @@
                 <div class="mb-5">
                     <label class="form-control w-full max-w-xs">
                         <div class="label">
-                            <span class="label-text">Product Image 1</span>
+                            <span class="label-text font-bold">Product Image 1</span>
                         </div>
                         <input type="file" name="product_image_1" class="file-input file-input-bordered w-full max-w-xs" required />
                     </label>
@@ -59,7 +78,7 @@
                 <div class="mb-5">
                     <label class="form-control w-full max-w-xs">
                         <div class="label">
-                            <span class="label-text">Product Image 2</span>
+                            <span class="label-text font-bold">Product Image 2</span>
                         </div>
                         <input type="file" name="product_image_2" class="file-input file-input-bordered w-full max-w-xs" required />
                     </label>
@@ -67,7 +86,7 @@
                 <div class="mb-5">
                     <label class="form-control w-full max-w-xs">
                         <div class="label">
-                            <span class="label-text">Product Image 3</span>
+                            <span class="label-text font-bold">Product Image 3</span>
                         </div>
                         <input type="file" name="product_image_3" class="file-input file-input-bordered w-full max-w-xs" required />
                     </label>
@@ -75,7 +94,7 @@
                 <div class="mb-5">
                     <label class="form-control w-full max-w-xs">
                         <div class="label">
-                            <span class="label-text">Product Price</span>
+                            <span class="label-text font-bold">Product Price</span>
                         </div>
                         <input type="text" name="product_price" placeholder="Enter Price" class="input input-bordered w-full max-w-xs" autocomplete="off" required />
                     </label>
