3fdf5a06131a33e64f96a66e8a3f3d153e0a3332
Anik0047 <clananik@gmail.com>
2024-06-13 20:08:50 +0600

    login form working

--- user_area/user_login.php
+++ user_area/user_login.php
@@ -1,14 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Include Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Include DaisyUI CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <!-- Include custom CSS file -->
+    <link rel="stylesheet" href="user_style.css">
+    <!-- Include SweetAlert2 CSS for styling the alert -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <style>
+
+    </style>
 </head>
 
-<body>
-    <h1>user login</h1>
+<body class="flex items-center">
+    <div class="design">
+        <h1 class="text-center text-3xl font-bold pt-7">Login</h1>
+        <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
+            <div class="custom pt-5">
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Email</label>
+                    <input name="user_email" type="text" placeholder="Enter Your Email" class="border-black input input-bordered w-full  " required />
+                </div>
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Password</label>
+                    <input name="user_password" type="password" placeholder="Enter Your Password" class="border-black input input-bordered w-full  " required />
+                </div>
+                <div class="text-left">
+                    <a class="font-bold link" href="">Forgot Password</a>
+                </div>
+                <div>
+                    <input name="user_register" type="submit" class="btn btn-accent font-bold" value="Login">
+                    <p class="font-bold pt-2">Already have an account? <a class="text-red-700 link" href="user_register.php">Registration</a>
+                    </p>
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
--- user_area/user_register.php
+++ user_area/user_register.php
@@ -9,63 +9,63 @@
     <!-- Include DaisyUI CSS -->
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
     <!-- Include custom CSS file -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="user_style.css">
     <!-- Include SweetAlert2 CSS for styling the alert -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <style>
-    .custom {
-        width: 600px;
-        margin: 0 auto;
-        text-align: center;
 
-    }
-
-    .custom label {
-        display: block;
-        text-align: left;
-    }
     </style>
 </head>
 
 <body>
-    <h1 class="text-center text-3xl font-bold mt-7">Registration</h1>
-    <form action="" method="post" enctype="multipart/form-data">
-        <div class="custom mt-5">
-            <div class="mb-5">
-                <label class="mb-3 ps-2 font-bold" for="">Name</label>
-                <input type="text" placeholder="Enter Your Name" class="border-black input input-bordered w-full " />
+    <div class="design">
+        <h1 class="text-center text-3xl font-bold pt-7">Registration</h1>
+        <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
+            <div class="custom pt-5">
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Name</label>
+                    <input name="user_name" type="text" placeholder="Enter Your Name"
+                        class="border-black input input-bordered w-full  " required />
+                </div>
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Email</label>
+                    <input name="user_email" type="text" placeholder="Enter Your Email"
+                        class="border-black input input-bordered w-full  " required />
+                </div>
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Image</label>
+                    <input name="user_image" type="file" class="border-black file-input file-input-bordered w-full "
+                        required />
+                </div>
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Password</label>
+                    <input name="user_password" type="password" placeholder="Enter Your Password"
+                        class="border-black input input-bordered w-full  " required />
+                </div>
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Confirm Password</label>
+                    <input name="confirm_password" type="password" placeholder="Confirm Password"
+                        class="border-black input input-bordered w-full  " required />
+                </div>
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Address</label>
+                    <input name="user_address" type="text" placeholder="Enter Your Address"
+                        class="border-black input input-bordered w-full  " required />
+                </div>
+                <div class="mb-5">
+                    <label class="mb-3 ps-2 font-bold" for="">Contact</label>
+                    <input name="user_phone" type="text" placeholder="Enter Your Number"
+                        class="border-black input input-bordered w-full  " required />
+                </div>
+                <div>
+                    <input name="user_register" type="submit" class="btn btn-accent font-bold" value="Register">
+                    <p class="font-bold pt-2">Already have an account? <a class="text-red-700 link"
+                            href="user_login.php">Login</a>
+                    </p>
+                </div>
             </div>
-            <div class="mb-5">
-                <label class="mb-3 ps-2 font-bold" for="">Email</label>
-                <input type="text" placeholder="Enter Your Email" class="border-black input input-bordered w-full " />
-            </div>
-            <div class="mb-5">
-                <label class="mb-3 ps-2 font-bold" for="">Image</label>
-                <input type="file" class="border-black file-input file-input-bordered w-full" />
-            </div>
-            <div class="mb-5">
-                <label class="mb-3 ps-2 font-bold" for="">Password</label>
-                <input type="text" placeholder="Enter Your Password"
-                    class="border-black input input-bordered w-full " />
-            </div>
-            <div class="mb-5">
-                <label class="mb-3 ps-2 font-bold" for="">Confirm Password</label>
-                <input type="text" placeholder="Confirm Password" class="border-black input input-bordered w-full " />
-            </div>
-            <div class="mb-5">
-                <label class="mb-3 ps-2 font-bold" for="">Address</label>
-                <input type="text" placeholder="Enter Your Address" class="border-black input input-bordered w-full " />
-            </div>
-            <div class="mb-5">
-                <label class="mb-3 ps-2 font-bold" for="">Contact</label>
-                <input type="text" placeholder="Enter Your Number" class="border-black input input-bordered w-full " />
-            </div>
-            <div>
-                <input type="submit" class="btn btn-accent" value="Submit">
-                <p class="font-bold mt-2">Already have an account? <a class="text-red-700 link" href="">Login</a></p>
-            </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </body>
 
 </html>
