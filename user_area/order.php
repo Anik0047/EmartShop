f4614790e8a7eed31002beb896a1d796e4586c02
Anik0047 <clananik@gmail.com>
2024-06-18 20:51:25 +0600

    working!

--- user_area/order.php
+++ user_area/order.php
@@ -2,4 +2,36 @@
 include('../database/connect.php');
 include('../functions/common_function.php');
 
-echo 'hello';
+if (isset($_GET['user_id'])) {
+    $user_id = $_GET['user_id'];
+}
+
+// getting total items and total price of product
+
+$ip_address = getIPAddress();
+
+$total_price = 0;
+
+$cart_query = "SELECT * from `cart_details` where ip_address='$ip_address'";
+
+$cart_result = mysqli_query($conn, $cart_query);
+
+$products_count = mysqli_num_rows($cart_result);
+
+while ($row_price = mysqli_fetch_array($cart_result)) {
+
+    $product_id = $row_price['product_id'];
+
+    $product_query = "SELECT * from `products` where product_id='$product_id'";
+
+    $product_result = mysqli_query($conn, $product_query);
+
+    while ($row_product_price = mysqli_fetch_array($product_result)) {
+
+        $product_price = array($row_product_price['product_price']);
+
+        $product_values = array_sum($product_price);
+
+        $total_price = $total_price + $product_values;
+    }
+}
