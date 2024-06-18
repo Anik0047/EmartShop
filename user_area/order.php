35a662ca7667a5f9e8b83a7eb12d1a109acbf5d3
Anik0047 <clananik@gmail.com>
2024-06-18 21:26:48 +0600

    inserted into database

--- user_area/order.php
+++ user_area/order.php
@@ -18,6 +18,10 @@ $cart_result = mysqli_query($conn, $cart_query);
 
 $products_count = mysqli_num_rows($cart_result);
 
+$invoice_number = mt_rand();
+
+$status = "pending";
+
 while ($row_price = mysqli_fetch_array($cart_result)) {
 
     $product_id = $row_price['product_id'];
@@ -34,4 +38,38 @@ while ($row_price = mysqli_fetch_array($cart_result)) {
 
         $total_price = $total_price + $product_values;
     }
+}
+
+
+// getting quantity form cart
+
+$get_cart = "SELECT * from `cart_details`";
+
+$cart_details_result = mysqli_query($conn, $get_cart);
+
+$get_item_quantity = mysqli_fetch_array($cart_details_result);
+
+$quantity = $get_item_quantity['quantity'];
+
+if ($quantity == 1) {
+
+    $quantity = 1;
+
+    $subtotal = $total_price;
+} else {
+
+    $quantity = $quantity;
+
+    $subtotal = $total_price * $quantity;
+}
+
+
+$insert_orders = "INSERT into `user_orders` (user_id,amount_due,invoice_number,total_products,order_date,order_status) values ($user_id,$subtotal,$invoice_number,$products_count,NOW(),'$status')";
+
+$result_query = mysqli_query($conn, $insert_orders);
+
+if ($result_query) {
+
+    echo "<script>alert('Orders are submitted successfully')</script>";
+    echo "<script>window.open('user_dashboard.php','_self')</script>";
 }
