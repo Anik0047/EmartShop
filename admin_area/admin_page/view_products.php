18d93dae8960c716c4c683eee61acab767e432e5
Anik0047 <clananik@gmail.com>
2024-06-21 22:21:24 +0600

    woking adming page

--- admin_area/admin_page/view_products.php
+++ admin_area/admin_page/view_products.php
@@ -2,16 +2,18 @@
     Products
 </h1>
 
-<div class="overflow-x-auto">
-    <table class="table">
+<div class="overflow-x-auto mt-10">
+    <table class="table text-center">
         <!-- head -->
         <thead>
             <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-                <th></th>
+                <th>Product Id</th>
+                <th>Product</th>
+                <th>Product Price</th>
+                <th>Total Sold</th>
+                <th>Status</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
         </thead>
         <tbody>
@@ -22,8 +24,7 @@
                     <div class="flex items-center gap-3">
                         <div class="avatar">
                             <div class="mask mask-squircle w-12 h-12">
-                                <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
-                                    alt="Avatar Tailwind CSS Component" />
+                                <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
                             </div>
                         </div>
                         <div>
@@ -31,11 +32,14 @@
                         </div>
                     </div>
                 </td>
-                <td>
-                </td>
-                <td></td>
+                <td><span class="text-lg font-bold">100</span> BDT</td>
+                <td>0</td>
+                <td>true</td>
                 <th>
-                    <button class="btn btn-ghost btn-xs">details</button>
+                    <a class="btn btn-ghost btn-xs"><i class="fa-solid fa-pen-to-square text-lg"></i></a>
+                </th>
+                <th>
+                    <a class="btn btn-ghost btn-xs"><i class="fa-solid fa-trash text-lg"></i></a>
                 </th>
             </tr>
         </tbody>
