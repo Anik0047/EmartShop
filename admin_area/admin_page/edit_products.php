6ef5e7185d575cdf7675b638c68d70ed54ffe594
Anik0047 <clananik@gmail.com>
2024-06-22 20:47:25 +0600

    edit page

--- admin_area/admin_page/edit_products.php
+++ admin_area/admin_page/edit_products.php
@@ -3,46 +3,61 @@
 </h1>
 
 <form action="" method="post" enctype="multipart/form-data">
-    <div class="w-96 mx-auto mb-5">
-        <span class="text-lg font-bold">Product Name</span>
-        <input name="product_name" type="text" value="" class="input input-bordered w-full" required />
-    </div>
-    <div class="w-96 mx-auto mb-5">
-        <span class="text-lg font-bold">Product Description</span>
-        <input name="product_description" type="text" value="" class="input input-bordered w-full" required />
-    </div>
-    <div class="w-96 mx-auto mb-5">
-        <span class="text-lg font-bold">Product Keyword</span>
-        <input name="product_keyword" type="text" value="" class="input input-bordered w-full" required />
-    </div>
-    <div class="w-96 mx-auto mb-5">
-        <select name="product_category" class="select select-bordered w-full" required>
-            <option disabled selected>Select Product Category</option>
-            <option value="Bkash">Shoes</option>
-        </select>
-    </div>
-    <div class="w-96 mx-auto mb-5">
-        <select name="product_brand" class="select select-bordered w-full" required>
-            <option disabled selected>Select Product Brand</option>
-            <option value="Bkash">Adidas</option>
-        </select>
-    </div>
-    <div class="w-96 mx-auto mb-5 flex">
-        <input name="product_image_1" type="file" class="file-input file-input-bordered w-full" />
-        <div class='mask mask-squircle w-16 h-16'>
-            <img src='./product_images/ADILETTE 22 SLIDES-pic-1.png' alt='Avatar Tailwind CSS Component' />
+    <div class="flex justify-evenly items-center">
+        <div>
+            <div class="w-96 mx-auto mb-5">
+                <span class="text-lg font-bold">Product Name</span>
+                <input name="product_name" type="text" value="" class="input input-bordered w-full" required />
+            </div>
+            <div class="w-96 mx-auto mb-5">
+                <span class="text-lg font-bold">Product Description</span>
+                <input name="product_description" type="text" value="" class="input input-bordered w-full" required />
+            </div>
+            <div class="w-96 mx-auto mb-5">
+                <span class="text-lg font-bold">Product Keyword</span>
+                <input name="product_keyword" type="text" value="" class="input input-bordered w-full" required />
+            </div>
+            <div class="w-96 mx-auto mb-5">
+                <select name="product_category" class="select select-bordered w-full" required>
+                    <option disabled selected>Select Product Category</option>
+                    <option value="Bkash">Shoes</option>
+                </select>
+            </div>
+            <div class="w-96 mx-auto mb-5">
+                <select name="product_brand" class="select select-bordered w-full" required>
+                    <option disabled selected>Select Product Brand</option>
+                    <option value="Bkash">Adidas</option>
+                </select>
+            </div>
+        </div>
+        <div>
+            <div class="w-96 mx-auto mb-5 flex">
+                <input name="product_image_1" type="file" class="file-input file-input-bordered w-full" />
+                <div class='mask mask-squircle w-16 h-16'>
+                    <img src='./product_images/ADILETTE 22 SLIDES-pic-1.png' alt='Avatar Tailwind CSS Component' />
+                </div>
+            </div>
+            <div class="w-96 mx-auto mb-5 flex">
+                <input name="product_image_2" type="file" class="file-input file-input-bordered w-full" />
+                <div class='mask mask-squircle w-16 h-16'>
+                    <img src='./product_images/ADILETTE 22 SLIDES-pic-2.png' alt='Avatar Tailwind CSS Component' />
+                </div>
+            </div>
+            <div class="w-96 mx-auto mb-5 flex">
+                <input name="product_image_3" type="file" class="file-input file-input-bordered w-full" />
+                <div class='mask mask-squircle w-16 h-16'>
+                    <img src='./product_images/ADILETTE 22 SLIDES-pic-3.png' alt='Avatar Tailwind CSS Component' />
+                </div>
+            </div>
+            <div class="w-96 mx-auto mb-5">
+                <span class="text-lg font-bold">Product Price</span>
+                <input name="product_price" type="text" value="" class="input input-bordered w-full" required />
+            </div>
         </div>
     </div>
-    <div class="w-96 mx-auto mb-5 flex">
-        <input name="product_image_2" type="file" class="file-input file-input-bordered w-full" />
-        <div class='mask mask-squircle w-16 h-16'>
-            <img src='./product_images/ADILETTE 22 SLIDES-pic-2.png' alt='Avatar Tailwind CSS Component' />
-        </div>
-    </div>
-    <div class="w-96 mx-auto mb-5 flex">
-        <input name="product_image_3" type="file" class="file-input file-input-bordered w-full" />
-        <div class='mask mask-squircle w-16 h-16'>
-            <img src='./product_images/ADILETTE 22 SLIDES-pic-3.png' alt='Avatar Tailwind CSS Component' />
-        </div>
+    <div class="w-96 mx-auto text-center my-10">
+        <input name="product_update" type="submit"
+            class='px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:shadow-lg focus:outline-none hover:bounce-once'
+            value="Update">
     </div>
 </form>
--- admin_area/index.php
+++ admin_area/index.php
@@ -17,8 +17,33 @@ include('../functions/common_function.php');
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+    @keyframes bounce-once {
+
+        0%,
+        100% {
+            transform: translateY(0);
+        }
+
+        50% {
+            transform: translateY(-10px);
+        }
+    }
+
+    .hover\:bounce-once:hover {
+        animation: bounce-once 0.5s ease;
+    }
+
+    .custom {
+        margin-left: 800px;
+    }
+    </style>
 </head>
 
 <body>
@@ -69,7 +94,8 @@ include('../functions/common_function.php');
                         View Products
                     </div>
                     <div class="collapse-content text-lg">
-                        <a class="block py-5" href="index.php?view_products"><i class="fa-solid fa-bolt pe-5"></i>Products</a>
+                        <a class="block py-5" href="index.php?view_products"><i
+                                class="fa-solid fa-bolt pe-5"></i>Products</a>
                         <a class="block py-5" href=""><i class="fa-solid fa-bolt pe-5"></i>Categories</a>
                         <a class="block py-5" href=""><i class="fa-solid fa-bolt pe-5"></i>Brands</a>
                     </div>
@@ -80,9 +106,12 @@ include('../functions/common_function.php');
                         Insert Products
                     </div>
                     <div class="collapse-content text-lg">
-                        <a class="block py-5" href="index.php?insert_product"><i class="fa-solid fa-bolt pe-5"></i>Products</a>
-                        <a class="block py-5" href="index.php?insert_categories"><i class="fa-solid fa-bolt pe-5"></i>Categories</a>
-                        <a class="block py-5" href="index.php?insert_brands"><i class="fa-solid fa-bolt pe-5"></i>Brands</a>
+                        <a class="block py-5" href="index.php?insert_product"><i
+                                class="fa-solid fa-bolt pe-5"></i>Products</a>
+                        <a class="block py-5" href="index.php?insert_categories"><i
+                                class="fa-solid fa-bolt pe-5"></i>Categories</a>
+                        <a class="block py-5" href="index.php?insert_brands"><i
+                                class="fa-solid fa-bolt pe-5"></i>Brands</a>
                     </div>
                 </div>
                 <div class="collapse collapse-plus">
